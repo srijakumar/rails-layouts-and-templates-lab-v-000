b108009d118f841b4767244892e365f66bc42196
Srija S <srijakumar7@gmail.com>
2019-05-27 17:31:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'apps/new'
+
+  get 'apps/create'
+
+  get 'apps/update'
+
+  get 'apps/edit'
+
+  get 'apps/destroy'
+
+  get 'apps/show'
+
+  get 'apps/index'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
